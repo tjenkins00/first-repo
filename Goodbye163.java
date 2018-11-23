17f5529a5b85548cc4d1066642fb18695094e8d0
unknown <you@example.com>
2018-11-22 22:08:04 -0500

    commit first program

---
+++ Goodbye163.java
@@ -0,0 +1,14 @@
+/**
+@author tmjenkins@aggies.ncat.edu
+*
+*
+* This program pronts out the stement "Goodbye COMP 163".
+**/
+public class Goodbye163 {
+public static void main(String[] args) {
+
+
+  System.out.println("Goodbye COMP 163");
+  
+  }
+}
